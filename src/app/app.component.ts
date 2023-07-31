f7cb5f64adba3ad69ecd904e0e1566782784d1be
shreyas.kulkarni <shreyas.kulkarni@in.ncs-i.com>
2023-07-31 17:39:31 +0530

    31/7/23 latest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -261,4 +261,43 @@ async getStockinfo(sym:string):Promise<number>{
       layout: 'lightHorizontalLines',
     };
   }
+
+
+  generatePDF() {
+    
+
+
+    // Convert the chart to an image using html2canvas
+    html2canvas(this.elementRef.nativeElement).then((canvas) => {
+      // Generate the PDF with the embedded pie chart image
+      const chartDataUrl = canvas.toDataURL();
+      const documentDefinition = this.getDocumentDDefinition(chartDataUrl);
+
+      pdfMake.createPdf(documentDefinition).open(); // Open the PDF in a new window for export
+    });
+  }
+
+  getDocumentDDefinition(chartDataUrl: string) {
+    const content = [
+      { text: 'PDF with Embedded Pie Chart', style: 'header' },
+      { text: 'Sample Pie Chart:', style: 'subheader' },
+      { image: chartDataUrl, width: 350 },
+    ];
+
+    const styles = {
+      header: {
+        fontSize: 18,
+        bold: true,
+        alignment: 'center' as Alignment,
+        margin: [0, 0, 0, 10] as Margins,
+      },
+      subheader: {
+        fontSize: 14,
+        bold: true,
+        margin: [0, 20, 0, 10] as Margins,
+      },
+    };
+
+    return { content, styles };
+  }
 }
